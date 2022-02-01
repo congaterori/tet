acf1405052457d123447e5bbfc2826d94376c62b
johnny20061234 <46470547+congaterori@users.noreply.github.com>
2022-02-01 11:00:47 +0700

    Update tet.bat

--- tet/tet.bat
+++ tet/tet.bat
@@ -3,11 +3,11 @@ title tet
 color a
 cls
 color 6e
-type C:\Users\%Username%\Desktop\tet\money\logo.txt
+type .\money\logo.txt
 echo.
 timeout 10 > nul
 color a
-type C:\Users\%Username%\Desktop\tet\luckeymoney.txt
+type .\luckeymoney.txt
 echo.
 pause
 cls
@@ -15,13 +15,13 @@ goto li_xi
 :li_xi
 cls
 color c
-type C:\Users\%Username%\Desktop\tet\li_xi.txt
+type .\li_xi.txt
 timeout 1 > nul
 cls
-type C:\Users\%Username%\Desktop\tet\li_xi_2.txt
+type .\li_xi_2.txt
 timeout 2 > nul
 cls
-type C:\Users\%Username%\Desktop\tet\li_xi.txt
+type .\li_xi.txt
 SETLOCAL EnableDelayedExpansion
 for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do (
   set "DEL=%%a"
@@ -38,7 +38,7 @@ if %ERRORLEVEL% == 3 goto exit
 :open
 cls
 color c
-type C:\Users\%Username%\Desktop\tet\li_xi_open.txt
+type .\li_xi_open.txt
 echo.
 echo loading...
 timeout 5 > nul
@@ -64,30 +64,28 @@ rem type 50dola.txt
 if %select% == 6 goto 5
 rem type 100dola.txt
 if %select% == 7 goto 6
-rem type combo100dola.txt
 if %select% == 8 goto 7
-rem type fivedola_gold.txt
 goto showme
 :1
-type C:\Users\%Username%\Desktop\tet\money\1bigdola.txt
+type .\money\1bigdola.txt
 goto next
 :2
-goto type C:\Users\%Username%\Desktop\tet\money\1dola.txt
+goto type .\money\1dola.txt
 goto next
 :3
-type C:\Users\%Username%\Desktop\tet\money\1dola_old.txt
+type .\money\1dola_old.txt
 goto next
 :4
-type C:\Users\%Username%\Desktop\tet\money\50dola.txt
+type .\money\50dola.txt
 goto next
 :5
-type C:\Users\%Username%\Desktop\tet\money\100dola.txt
+type .\money\100dola.txt
 goto next
 :6
-type C:\Users\%Username%\Desktop\tet\money\combo100dola.txt
+type .\money\combo100dola.txt
 goto next
 :7
-type C:\Users\%Username%\Desktop\tet\money\fivedola_gold.txt
+type .\money\fivedola_gold.txt
 goto next
 :next
 color a
@@ -96,7 +94,6 @@ pause > nul
 cls
 goto li_xi
 :ColorText
-echo off
 <nul set /p ".=%DEL%" > "%~2"
 findstr /v /a:%1 /R "^$" "%~2" nul
 del "%~2" > nul 2>&1
